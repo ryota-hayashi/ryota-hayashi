68bcc74330997daaba4c11c71eae2e62dd0beeb3
ryota_hayashi <ryota.hayashi1116@gmail.com>
2019-12-07 10:43:23 +0900

    課題提出 PHP-データ型

---
+++ dataType/taskDataType.php
@@ -0,0 +1,22 @@
+<?php
+    $intNum = 5;
+    $str1 = "string型を使用するときに、ダブルクオート(\")を表示する場合は、バックスラッシュ(\)が必要です。";
+    $str2 = 'I\'m Security Engineer';
+    const greeting = "こんばんは";
+    $name = "太郎くん";
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskDataType</title>
+</head>
+<body>
+    <p><?php echo("りんごが"."$intNum"."個あります。"); ?></p>
+    <p><?php echo($str1); ?></p>
+    <p><?php echo($str2); ?></p>
+    <p><?php echo(greeting."、".$name); ?></p>
+</body>
+</html>
