3e22862b9ae46e1a9abd5b6ece13c40edcfa4239
ryota_hayashi <ryota.hayashi1116@gmail.com>
2020-01-25 17:12:34 +0900

    データ型 レビュー修正

--- dataType/taskDataType.php
+++ dataType/taskDataType.php
@@ -1,8 +1,8 @@
 <?php
     $intNum = 5;
-    $str1 = "string型を使用するときに、ダブルクオート(\")を表示する場合は、バックスラッシュ(\)が必要です。";
+    $str1 = "string型を使用するときに、ダブルクオート(\")を表示する場合は、バックスラッシュ(\\)が必要です。";
     $str2 = 'I\'m Security Engineer';
-    const greeting = "こんばんは";
+    const GREETING = "こんばんは";
     $name = "太郎くん";
 ?>
 <!DOCTYPE html>
@@ -17,6 +17,6 @@
     <p><?php echo("りんごが" . "$intNum" . "個あります。"); ?></p>
     <p><?php echo($str1); ?></p>
     <p><?php echo($str2); ?></p>
-    <p><?php echo(greeting . "、" . $name); ?></p>
+    <p><?php echo(GREETING . "、" . $name); ?></p>
 </body>
 </html>
