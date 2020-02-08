901bc8a1d169ce51602a1a062f3546b032a618a4
ryota_hayashi <ryota.hayashi1116@gmail.com>
2020-02-08 16:34:29 +0900

    課題提出 PHP-データ削除

---
+++ db/delete.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskCalc</title>
+</head>
+<body>
+<!-- 課題1-1 -->
+    <p><?php echo(
+        "delete from heritage where name in ('小笠原諸島', '姫路城');"
+    ); ?></p>
+</body>
+<body>
+</body>
+</html>
