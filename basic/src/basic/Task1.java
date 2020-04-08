ff7c9d92b6b468b02930eab7cee38bb9c29d0273
ryota-hayashi <ryota.hayashi1116@gmail.com>
2020-04-08 17:39:12 +0900

    課題提出 - Javaの基礎

--- basic/src/basic/Task1.java
+++ basic/src/basic/Task1.java
@@ -4,7 +4,8 @@ public class Task1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println("初めての課題");
+		System.out.print("Java攻略の道");
 	}
 
 }
---
+++ basic/src/basic/Task2.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		/*コメント
+		 コメント*/
+		System.out.println("hogehoge");
+		System.out.print("hogehoge2");
+	}
+}
