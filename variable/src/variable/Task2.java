e576871264d3acb72b9648cb60e4041c0cfcc09a
ryota-hayashi <ryota.hayashi1116@gmail.com>
2020-04-13 10:42:35 +0900

    再提出 -変数・定数

--- variable/src/variable/Task2.java
+++ variable/src/variable/Task2.java
@@ -4,10 +4,10 @@ public class Task2 {
 
 	public static void main(String[] args) {
 		String name = "山田　太郎";
-		String phoneNumber = "090-9999-1111";
+		final String PHONE_NUMBER = "090-9999-1111";
 
 		System.out.println(name + "です。");
-		System.out.print("私の電話番号は\'" + phoneNumber + "\'です。");
+		System.out.print("私の電話番号は\'" + PHONE_NUMBER + "\'です。");
 	}
 
 }
