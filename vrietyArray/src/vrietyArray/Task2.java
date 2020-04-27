81c4b6d6480bec08b4090beb31837c96e611837b
ryota-hayashi <ryota.hayashi1116@gmail.com>
2020-04-27 21:56:16 +0900

    再提出 - いろいろな配列

--- vrietyArray/src/vrietyArray/Task2.java
+++ vrietyArray/src/vrietyArray/Task2.java
@@ -7,7 +7,7 @@ public class Task2 {
 	public static void main(String[] args) {
 
 		//配列定義
-		int[] array = {21,3,32,6,99,72,78,51,1,26,87,11,48,60};
+		int[] array = {21, 3, 32, 6, 99, 72, 78, 51, 1, 26, 87, 11, 48, 60};
 
 		//奇数ArrayList定義
 		ArrayList<Integer> oddNumbers = new ArrayList<>();
@@ -30,7 +30,17 @@ public class Task2 {
 		}
 
 		//奇数の表示
-		System.out.print("奇数: [");
+		System.out.println("奇数: " + oddNumbers);
+
+		//偶数の表示
+		System.out.println("偶数: " + evenNumbers);
+
+
+
+		//以下ループを使用した表示
+
+		//奇数の表示
+		/*System.out.print("奇数: [");
 
 		for(int i = 0; i < oddNumbers.size(); i++) {
 
@@ -61,7 +71,7 @@ public class Task2 {
 
 		}
 
-		System.out.println("]");
+		System.out.println("]");*/
 
 	}
 
